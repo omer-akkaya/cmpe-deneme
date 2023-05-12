ad25071d40219f49b2a8d50ffdfe745cff8c3101
omer akkaya <farukakkayaya51@gmail.com>
2023-05-12 18:23:52 +0300

    updated product.php page

--- product.php
+++ product.php
@@ -4,3 +4,34 @@ if (!isset($_SESSION["id"])) {
     exit();
 }
 ?>
+
+<!DOCTYPE html>
+
+<head>
+    <title>Product Page</title>
+    <?php
+    include "js/product.js.php";
+    include "styles/product.css.php";
+    ?>
+</head>
+
+<body>
+    <!-- Header starts -->
+    <?php include "includes/header.php";
+    include "styles/header.css.php";
+    include "js/header.js.php"; ?>
+    <!-- Header ends -->
+
+    <!--Main section starts-->
+    <div class="main">
+        <img id="image" />
+        <div class="main-information">
+            <div id="title"></div>
+            <div id="price"></div>
+            <div id="description"></div>
+        </div>
+    </div>
+    <!--Main section ends-->
+</body>
+
+</html>
